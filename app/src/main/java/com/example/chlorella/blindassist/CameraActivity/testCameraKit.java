2d448d6b21e544eeea4ea03816a235e20a35e3ae
chlorella <chlorella2012@gmail.com>
2017-03-24 16:47:43 +0800

    Success Camerakit start

--- app/src/main/java/com/example/chlorella/blindassist/CameraActivity/testCameraKit.java
+++ app/src/main/java/com/example/chlorella/blindassist/CameraActivity/testCameraKit.java
@@ -2,7 +2,6 @@ package com.example.chlorella.blindassist.CameraActivity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.example.chlorella.blindassist.R;
 import com.flurgle.camerakit.CameraView;
@@ -14,7 +13,7 @@ import butterknife.ButterKnife;
  * Created by chlorella on 23/3/2017.
  */
 
-public class testCameraKit extends AppCompatActivity implements View.OnLayoutChangeListener {
+public class testCameraKit extends AppCompatActivity{
 
     @BindView(R.id.camera)
     CameraView camera;
@@ -24,19 +23,8 @@ public class testCameraKit extends AppCompatActivity implements View.OnLayoutCha
         super.onCreate(savedInstanceState);
         setContentView(R.layout.testcamerakit);
         ButterKnife.bind(this);
-
-        camera.addOnLayoutChangeListener(this);
     }
 
-    public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-        mCameraWidth = right - left;
-        mCameraHeight = bottom - top;
-
-        width.setText(String.valueOf(mCameraWidth));
-        height.setText(String.valueOf(mCameraHeight));
-
-        camera.removeOnLayoutChangeListener(this);
-    }
 
     protected void onResume() {
         super.onResume();
