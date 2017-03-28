9930b411c1ff216b0ca76ce47feab2db8ebf9081
chlorella <chlorella2012@gmail.com>
2017-03-28 17:20:29 +0800

    camerakit layout
    fix button question
    modify layout
    add album button
    add light button(on going?)
    capture with imageView

--- app/src/main/java/com/example/chlorella/blindassist/CameraActivity/testCameraKit.java
+++ app/src/main/java/com/example/chlorella/blindassist/CameraActivity/testCameraKit.java
@@ -1,17 +1,19 @@
 package com.example.chlorella.blindassist.CameraActivity;
 
-import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
 import com.example.chlorella.blindassist.R;
 import com.example.chlorella.blindassist.helper.FocusMarkerLayout;
+import com.flurgle.camerakit.CameraKit;
 import com.flurgle.camerakit.CameraListener;
 import com.flurgle.camerakit.CameraView;
 
@@ -25,11 +27,17 @@ import butterknife.OnTouch;
  * Created by chlorella on 23/3/2017.
  */
 
-public class testCameraKit extends AppCompatActivity {
+public class testCameraKit extends AppCompatActivity implements View.OnLayoutChangeListener {
     @BindView(R.id.camera)
     CameraView camera;
     @BindView(R.id.focusMarker)
     FocusMarkerLayout focusMarker;
+    @BindView(R.id.imageView)
+    ImageView imageView;
+    @BindView(R.id.toggleFlash)
+    ToggleButton toggleFlash;
+    @BindView(R.id.album)
+    Button album;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +45,35 @@ public class testCameraKit extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @OnTouch(R.id.focusMarker)
+    boolean onTouchCamera(View view, MotionEvent motionEvent) {
+        focusMarker.focus(motionEvent.getX(), motionEvent.getY());
+        return false;
+    }
+
+    @OnClick(R.id.capture)
+    void capturePhoto() {
+        camera.setCameraListener(new CameraListener() {
+            @Override
+            public void onPictureTaken(byte[] picture) {
+                super.onPictureTaken(picture);
+
+                // Create a bitmap
+                Bitmap result = BitmapFactory.decodeByteArray(picture, 0, picture.length);
+                imageView.setImageBitmap(result);
+            }
+        });
+        camera.captureImage();
+    }
+
+    @OnClick(R.id.toggleFlash)
+    void toggleFlash() {
+        if (toggleFlash.isChecked()) {
+            camera.setFlash(CameraKit.Constants.FLASH_AUTO);
+        } else {
+            camera.setFlash(CameraKit.Constants.FLASH_OFF);
+        }
+    }
 
     protected void onResume() {
         super.onResume();
@@ -49,31 +86,16 @@ public class testCameraKit extends AppCompatActivity {
         super.onPause();
     }
 
-    @OnClick(R.id.kit_capture)
-    void capturePhoto(View view) {
-        Log.d("Capture", "in");
-        camera.setCameraListener(new CameraListener() {
-            @Override
-            public void onPictureTaken(byte[] picture) {
-                super.onPictureTaken(picture);
-                Log.d("Capture", "onPictureTaken");
-                // Create a bitmap
-                Bitmap result = BitmapFactory.decodeByteArray(picture, 0, picture.length);
-            }
-        });
-        camera.captureImage();
+    @Override
+    public void onLayoutChange(View view, int i, int i1, int i2, int i3, int i4, int i5, int i6, int i7) {
 
-        Context context = getApplicationContext();
-        int duration = Toast.LENGTH_SHORT;
-        CharSequence text = "touch";
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
     }
 
-    @OnTouch(R.id.focusMarker)
-    boolean onTouchCamera(View view, MotionEvent motionEvent) {
-        focusMarker.focus(motionEvent.getX(), motionEvent.getY());
-        return false;
+    @OnClick(R.id.album)
+    public void onViewClicked() {
+        Intent getImage = new Intent(Intent.ACTION_GET_CONTENT);
+        getImage.addCategory(Intent.CATEGORY_OPENABLE);
+        getImage.setType("image/jpeg");
+        testCameraKit.this.startActivity(getImage);
     }
 }
